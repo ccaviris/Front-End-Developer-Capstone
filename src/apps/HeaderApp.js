9086c03b53c229645774c827d4b3169c513b00b1
Costa <costaCosta@MacBook.local>
2024-11-11 14:08:23 -0500

    Update header metadata

--- src/apps/HeaderApp.js
+++ src/apps/HeaderApp.js
@@ -4,6 +4,10 @@
 function Header() {
     return (
       <header>
+        <meta name="description" content="The official website for the Little Lemon restaurant."/>
+        <meta name="og:title" content="The Little Lemon"/>
+        <meta name="og:description" content="The official website for the Little Lemon restaurant."/>
+        <meta name="og:image" content="https://example.com/one-place-fishing-store.jpg"/>
 
       </header>
     );
@@ -13,5 +17,5 @@ function Header() {
 
   /*
   TODO:
-
+    Update URL for og:image
   */
