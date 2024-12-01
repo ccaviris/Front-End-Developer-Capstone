d2af1ed41e9336be5b5e475ae396c91d7fe3aa39
Costa <costaCosta@MacBook.local>
2024-12-01 17:04:11 -0500

    Updated other pages

---
+++ src/apps/ChicagoApp.js
@@ -0,0 +1,9 @@
+function ChicagoApp() {
+    return <section><article>
+    <h1>About Us</h1>
+    <p>The little lemon. We want your money. We'll get it by selling you what some might consider food.
+    </p>
+  </article></section>
+}
+
+export default ChicagoApp;
--- src/apps/Main.js
+++ src/apps/Main.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomeApp from './HomeApp.js'
 import BookingPageApp from './BookingPageApp.js'
+import ChicagoApp from './ChicagoApp.js'
+import SpecialsApp from './SpecialsApp.js'
 
 
 
@@ -13,6 +15,9 @@ function Main() {
           <Route path="/" element={<HomeApp/>}></Route>
           <Route path="/home" element={<HomeApp/>}></Route>
           <Route path="/reservations" element={<BookingPageApp/>}></Route>
+          <Route path="/about" element={<ChicagoApp/>}></Route>
+          <Route path="/menu" element={<SpecialsApp/>}></Route>
+          <Route path="/order" element={<SpecialsApp/>}></Route>
         </Routes>
         </BrowserRouter>
         <div>This is the main element</div>
